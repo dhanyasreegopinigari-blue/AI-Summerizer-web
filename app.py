4d6a5e1cf9c1da161ed1552a3bc4fddecc898129
Dhanya Sree <dhanyasreegopinigari@gmail.com>
2026-06-01 18:27:04 +0530

    Errors fixing

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import (
     Flask,
     render_template,
@@ -425,8 +427,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-import os
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
