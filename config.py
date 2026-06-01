b1a24afcb818154d95a7197db2a64bb2f53f965e
Dhanya Sree <dhanyasreegopinigari@gmail.com>
2026-06-01 17:04:00 +0530

    Config changes

--- config.py
+++ config.py
@@ -12,6 +12,8 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "your_email@gmail.com"
-    MAIL_PASSWORD = "your_gmail_app_password"
+
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
