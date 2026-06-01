3172d100b3c91c0fa492f6d2509bef443af572ae
Dhanya Sree <dhanyasreegopinigari@gmail.com>
2026-06-01 17:52:56 +0530

    Fixing issues

--- app.py
+++ app.py
@@ -425,4 +425,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(host="0.0.0.0", port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
